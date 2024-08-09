139a2b33b210449ce3942af810c02e199ee3ffef
arimar1002 <80643072+arimar1002@users.noreply.github.com>
2024-08-09 11:20:05 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -21,7 +21,16 @@ $('.owl-carousel').owlCarousel({
 
 
 
-
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
 
 
 
